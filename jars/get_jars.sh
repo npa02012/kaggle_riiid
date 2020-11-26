380f37302523e3f181d93aae0c4d8f34b7bf8df2
Ubuntu <ubuntu@ip-172-31-90-206.ec2.internal>
2020-11-26 21:03:01 +0000

    Script to download jars

---
+++ jars/get_jars.sh
@@ -0,0 +1,4 @@
+wget https://repo1.maven.org/maven2/org/apache/hadoop/hadoop-aws/3.2.0/hadoop-aws-3.2.0.jar
+
+wget https://repo1.maven.org/maven2/com/amazonaws/aws-java-sdk-bundle/1.11.908/aws-java-sdk-bundle-1.11.908.jar
+
